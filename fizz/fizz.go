238466bc2164d5846d553af4928dc934828201e9
Gnomeek <gnomeek@outlook.com>
2022-04-19 21:16:30 +0800

    :sparkles: add route grouping

--- fizz/fizz.go
+++ fizz/fizz.go
@@ -7,14 +7,25 @@ import (
 // HandlerFunc is customized based on http.HandlerFunc
 type HandlerFunc func(c *Context)
 
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	engine      *Engine
+}
+
 // Engine stores router info, etc.(TBD)
 type Engine struct {
-	router *router
+	*RouterGroup
+	router       *router
+	routerGroups []*RouterGroup
 }
 
 // New creates a new engine with router without routes
 func New() *Engine {
-	return &Engine{router: NewRouter()}
+	engine := &Engine{router: NewRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.routerGroups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
 // addRoute register handler into route,
@@ -43,4 +54,29 @@ func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 // Run starts an HTTP service
 func (engine *Engine) Run(addr string) error {
 	return http.ListenAndServe(addr, engine)
+}
+
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		engine: engine,
+	}
+	engine.routerGroups = append(engine.routerGroups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+// GET defines the method to add GET request
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
